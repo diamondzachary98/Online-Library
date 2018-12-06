b3221aa0ce6ad22338b4ba3dc106630c970048f6
Zachary Diamond <diamondzachary98@gmail.com>
2018-12-05 19:43:30 -0500

    Add files via upload

---
+++ online-library copy/inc/data.php
@@ -0,0 +1,187 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The New Teen Titans, Vol. 1",
+	"img" => "img/media/new-teen-titans-1.jpg",
+    "genre" => "Comics & Graphic Novels",
+    "format" => "Paperback",
+    "year" => 2014,
+    "category" => "Books",
+    "authors" => [
+        "Marv Wolfman",
+        "George Perez",
+        "Romeo Tanghal"
+    ],
+    "publisher" => "DC Comics",
+    "isbn" => '978-1401251437'
+];
+$catalog[102] = [
+    "title" => "The New Teen Titans, Vol. 2",
+    "img" => "img/media/new-teen-titans-2.jpg",
+    "genre" => "Comics & Graphic Novels",
+    "format" => "Paperback",
+    "year" => 2015,
+    "category" => "Books",
+    "authors" => [
+				"Marv Wolfman",
+				"George Perez",
+				"Romeo Tanghal"
+    ],
+    "publisher" => "DC Comics",
+    "isbn" => '978-1401255329'
+];
+$catalog[103] = [
+    "title" => "The New Teen Titans, Vol. 3",
+    "img" => "img/media/new-teen-titans-3.jpg",
+    "genre" => "Comics & Graphic Novels",
+    "format" => "Paperback",
+    "year" => 2015,
+    "category" => "Books",
+    "authors" => [
+				"Marv Wolfman",
+				"George Perez",
+				"Romeo Tanghal"
+    ],
+    "publisher" => "DC Comics",
+    "isbn" => '978-1401258542'
+];
+$catalog[104] = [
+    "title" => "The Complete Tales of Winnie-the-Pooh",
+    "img" => "img/media/winnie-the-pooh.jpg",
+    "genre" => "Children's Books",
+    "format" => "Hardcover",
+    "year" => 1994,
+    "category" => "Books",
+    "authors" => [
+        "A.A. Milne"
+    ],
+    "publisher" => "Dutton Children's Books",
+    "isbn" => '9780525457534'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Wonder Woman",
+    "img" => "img/media/wonder-woman.jpg",
+    "genre" => "Action & Adventure",
+    "format" => "Blu-Ray & Digital",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "Patty Jenkins",
+    "writers" => [
+				"Zack Snyder",
+	 			"Allan Heinberg",
+	 			"Jason Fuchs"
+    ],
+    "stars" => [
+			"Gal Gadot",
+			"Chris Pine",
+			"Robin Wright",
+			"Connie Nielsen",
+			"Lucy Davis",
+			"David Thewlis"
+    ]
+];
+$catalog[202] = [
+    "title" => "The Little Mermaid",
+    "img" => "img/media/little-mermaid.jpg",
+    "genre" => "Kids & Family",
+    "format" => "Blu-Ray & Digital",
+    "year" => 1989,
+    "category" => "Movies",
+    "director" => "Ron Clements & John Musker",
+    "writers" => [
+			"John Musker",
+ 			"Ron Clements",
+ 			"Howard Ashman",
+ 			"Gerrit Graham",
+ 			"Sam Graham",
+ 			"Chris Hubbell"
+    ],
+    "stars" => [
+			"Jodi Benson",
+	 		"Pat Carroll",
+	 		"Samuel E. Wright",
+	 		"Kenneth Mars",
+	 		"Buddy Hackett"
+    ]
+];
+$catalog[203] = [
+    "title" => "Peter Pan",
+    "img" => "img/media/peter-pan.jpg",
+    "genre" => "Kids & Family",
+    "format" => "Blu-Ray & Digital",
+    "year" => 1953,
+    "category" => "Movies",
+    "director" => "Clyde Geronimi & Wilfred Jackson & Hamilton Luske",
+    "writers" => [
+				"Bill Cottrell",
+				"Winston Hibler",
+				"Bill Peet"
+    ],
+    "stars" => [
+				"Bobby Driscoll",
+	 			"Kathryn Beaumont",
+	 			"Paul Collins",
+	 			"Tommy Luske",
+	 			"Hans Conried",
+	 			"Heather Angel"
+    ]
+];
+$catalog[204] = [
+    "title" => "Spider-Man",
+    "img" => "img/media/spider-man.jpg",
+    "genre" => "Action & Adventure",
+    "format" => "Blu-Ray & Digital",
+    "year" => 2002,
+    "category" => "Movies",
+    "director" => "Sam Raimi",
+    "writers" => [
+        "David Koepp"
+    ],
+    "stars" => [
+				"Tobey Maguire",
+				"Willem Dafoe",
+				"Kirsten Dunst",
+				"James Franco",
+				"Cliff Robertson",
+				"Rosemary Harris"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Teenage Dream",
+    "img" => "img/media/katy-perry.jpg",
+    "genre" => "Pop",
+    "format" => "Digital",
+    "year" => 2010,
+    "category" => "Music",
+    "artist" => "Katy Perry"
+];
+$catalog[302] = [
+    "title" => "Animal",
+    "img" => "img/media/kesha.jpg",
+    "genre" => "Pop",
+    "format" => "Digital",
+    "year" => 2009,
+    "category" => "Music",
+    "artist" => "Kesha"
+];
+$catalog[303] = [
+    "title" => "Mulan (An Original Walt Disney Records Soundtrack)",
+    "img" => "img/media/mulan.jpg",
+    "genre" => "Soundtrack",
+    "format" => "Digital",
+    "year" => 1998,
+    "category" => "Music",
+    "artist" => "Various Artists"
+];
+$catalog[304] = [
+    "title" => "20th Century Masters - The Millennium Collection: The Best of Cher",
+    "img" => "img/media/cher.jpg",
+    "genre" => "Rock",
+    "format" => "Digital",
+    "year" => 2000,
+    "category" => "Music",
+    "artist" => "Cher"
+];
